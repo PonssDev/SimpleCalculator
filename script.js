dd8dbf6e666b416be315256439722309d037d908
Pons <alanponstrabajo@gmail.com>
2024-02-23 20:02:55 +0100

    Comentar código

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+// Obtiene la referencia al elemento con el id "pantalla" en el documento
 const pantalla = document.querySelector("#pantalla");
+
+// Obtiene una lista de todos los elementos con la clase "btn" en el documento
 const botones = document.querySelectorAll(".btn");
 
+// Itera sobre cada botón en la lista
 botones.forEach((boton) => {
+  // Agrega un evento de clic a cada botón
   boton.addEventListener("click", () => {
+    // Obtiene el texto contenido en el botón presionado
     const botonApretado = boton.textContent;
 
+    // Lógica para el botón "c" (limpiar pantalla)
     if (boton.id === "c") {
       pantalla.textContent = "0";
       return;
     }
 
+    // Lógica para el botón "borrar" (eliminar último carácter en pantalla)
     if (boton.id === "borrar") {
+      // Verifica si la pantalla tiene un solo carácter o muestra "Syntax Error"
       if (
         pantalla.textContent.length === 1 ||
         pantalla.textContent === "Syntax Error"
@@ -22,21 +31,27 @@ botones.forEach((boton) => {
       return;
     }
 
+    // Lógica para el botón "igual" (realiza la operación y muestra el resultado)
     if (boton.id === "igual") {
       try {
+        // Evalúa la expresión matemática en la pantalla y actualiza el contenido
         pantalla.textContent = eval(pantalla.textContent);
       } catch {
+        // Maneja errores de sintaxis mostrando "Syntax Error" en la pantalla
         pantalla.textContent = "Syntax Error";
       }
       return;
     }
 
+    // Lógica para los demás botones (números y operadores)
     if (
       pantalla.textContent === "0" ||
       pantalla.textContent === "Syntax Error"
     ) {
+      // Si la pantalla muestra "0" o "Syntax Error", reemplaza con el nuevo botón
       pantalla.textContent = botonApretado;
     } else {
+      // Agrega el texto del botón a la pantalla existente
       pantalla.textContent += botonApretado;
     }
   });
